29a8258766c4e8a58a9317b2a548598e56a97da9
wstaniszewski <w.staniszewski@gmail.com>
2021-07-03 19:11:01 +0200

    Update pxt.json, main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,17 +1,8 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    basic.showString("A")
-    serial.writeLine("Test A")
-})
-function zapal_LED(num: number, num2: number) {
-    led.plot(num, num2)
-}
-
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    basic.showString("B")
-    serial.writeLine("Test B")
-})
-let var_ = 4
-zapal_LED(var_, 1)
-input.onLogoDown(function on_logo_down() {
-    basic.showString("Hello!")
-})
+led.plotBarGraph(
+sonar.ping(
+DigitalPin.P0,
+DigitalPin.P0,
+PingUnit.Centimeters
+),
+0
+)
