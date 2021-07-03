8fd252287613ccbd1b0592ce03afa8c707bba412
wstaniszewski <w.staniszewski@gmail.com>
2021-07-03 19:39:22 +0200

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-sonar.ping(DigitalPin.P0, DigitalPin.P0, PingUnit.MICRO_SECONDS)
+distance = 0
+
+def on_button_pressed_a():
+    doSomething(10)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def doSomething(num: number):
+    global distance
+    distance = sonar.ping(DigitalPin.P0, DigitalPin.P1, PingUnit.CENTIMETERS)
+    led.plot_bar_graph(distance, 100)
+    if distance > num:
+        pins.digital_write_pin(DigitalPin.P2, 1)
+    else:
+        pins.digital_write_pin(DigitalPin.P2, 0)
+
+def on_button_pressed_b():
+    doSomething(100)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_forever():
+    pass
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,8 +1,22 @@
-led.plotBarGraph(
-sonar.ping(
-DigitalPin.P0,
-DigitalPin.P0,
-PingUnit.Centimeters
-),
-0
-)
+let distance = 0
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    doSomething(10)
+})
+function doSomething(num: number) {
+    
+    distance = sonar.ping(DigitalPin.P0, DigitalPin.P1, PingUnit.Centimeters)
+    led.plotBarGraph(distance, 100)
+    if (distance > num) {
+        pins.digitalWritePin(DigitalPin.P2, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P2, 0)
+    }
+    
+}
+
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    doSomething(100)
+})
+basic.forever(function on_forever() {
+    
+})
